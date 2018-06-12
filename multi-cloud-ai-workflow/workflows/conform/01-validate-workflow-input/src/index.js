f9c5d40443b3f8b5ebd853a0a50d1c5ec2fc09eb
sanom1969 <m.ework1969@gmail.com>
2018-06-12 17:33:13 +0900

    commit test by masa.

--- multi-cloud-ai-workflow/workflows/conform/01-validate-workflow-input/src/index.js
+++ multi-cloud-ai-workflow/workflows/conform/01-validate-workflow-input/src/index.js
@@ -1,9 +1,15 @@
-
+// require
 const AWS = require("aws-sdk")
 const async = require("async");
-
+// get reference to S3 client
 var s3 = new AWS.S3();
 
+/**
+ * Lambda function handler
+ * @param {*} event
+ * @param {*} context
+ * @param {*} callback
+ */
 exports.handler = (event, context, callback) => {
     console.log("Hello, Lambda!");
     console.log("S3 api version = " + s3.apiVersion);
