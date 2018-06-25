545c0c12c42759e63d139bf46d25cfd580cfdba4
sanom1969 <m.ework1969@gmail.com>
2018-06-25 18:43:32 +0900

    Made the folder structure for AI workflow. But not complete setting for 'build.gradle.' By masa

---
+++ multi-cloud-ai-workflow/workflows/ai/06a-detect-celebrities-aws/src/index.js
@@ -0,0 +1,13 @@
+// require
+const AWS = require("aws-sdk");
+const async = require("async");
+
+/**
+ * Lambda function handler
+ * @param {*} event event
+ * @param {*} context context
+ * @param {*} callback callback
+ */
+exports.handler = (event, context, callback) => {
+    return callback();
+}
