b09f1cdaf73526f4df6738e73ba258deba53bb5b
Joost Rovers <joost.rovers@glookast.com>
2018-07-09 11:26:44 +0100

    Added AME service. REST API works but there is no implementation of any business logic yet.

---
+++ multi-cloud-ai-workflow/services/ame-service/src/index.js
@@ -0,0 +1,108 @@
+//"use strict";
+
+const AWS = require("aws-sdk");
+const MCMA_AWS = require("mcma-aws");
+const uuidv4 = require('uuid/v4');
+
+// async functions to handle the different routes.
+
+async function getJobAssignments (request, response) {
+    console.log("getJobAssignments()", JSON.stringify(request, null, 2));
+
+    let table = new MCMA_AWS.DynamoDbTable(AWS, request.stageVariables.TableName);
+
+    response.body = await table.getAll("JobAssignment");
+    
+    console.log(JSON.stringify(response, null, 2));
+}
+
+async function addJobAssignment (request, response) {
+    console.log("addJobAssignment()", JSON.stringify(request, null, 2));
+
+    let jobAssignment = request.body;
+    if (!jobAssignment) {
+        response.statusCode = MCMA_AWS.HTTP_BAD_REQUEST;
+        response.statusMessage = "Missing request body.";
+        return;
+    }
+
+    let jobAssignmentId = uuidv4();
+    jobAssignment["@type"] = "JobAssignment";
+    jobAssignment.id = request.stageVariables.PublicUrl + "/job-assignments/" + jobAssignmentId;
+
+    let table = new MCMA_AWS.DynamoDbTable(AWS, request.stageVariables.TableName);
+
+    await table.put("JobAssignment", jobAssignmentId, jobAssignment);
+
+    response.statusCode = MCMA_AWS.HTTP_CREATED;
+    response.headers.Location = jobAssignment.id;
+    response.body = jobAssignment;
+
+    console.log(JSON.stringify(response, null, 2));
+}
+
+async function getJobAssignment (request, response) {
+    console.log("getJobAssignment()", JSON.stringify(request, null, 2));
+
+    let table = new MCMA_AWS.DynamoDbTable(AWS, request.stageVariables.TableName);
+    
+    response.body = await table.get("JobAssignment", request.pathVariables.id);
+
+    if (response.body === null) {
+        response.statusCode = MCMA_AWS.HTTP_NOT_FOUND;
+        response.statusMessage = "No resource found on path '" + request.path + "'.";
+    }
+}
+
+async function putJobAssignment (request, response) {
+    console.log("putJobAssignment()", JSON.stringify(request, null, 2));
+
+    let jobAssignment = request.body;
+    if (!jobAssignment) {
+        response.statusCode = MCMA_AWS.HTTP_BAD_REQUEST;
+        response.statusMessage = "Missing request body.";
+        return;
+    }
+
+    let jobAssignmentId = request.pathVariables.id;
+    jobAssignment["@type"] = "JobAssignment";
+    jobAssignment.id = request.stageVariables.PublicUrl + "/job-assignments/" + jobAssignmentId;
+
+    let table = new MCMA_AWS.DynamoDbTable(AWS, request.stageVariables.TableName);
+
+    await table.put("JobAssignment", jobAssignmentId, jobAssignment);
+
+    response.body = jobAssignment;
+}
+
+async function deleteJobAssignment (request, response) {
+    console.log("deleteJobAssignment()", JSON.stringify(request, null, 2));
+
+    let jobAssignmentId = request.pathVariables.id;
+    let table = new MCMA_AWS.DynamoDbTable(AWS, request.stageVariables.TableName);
+
+    let jobAssignment = await table.get("JobAssignment", jobAssignmentId);
+    if (!jobAssignment) {
+        response.statusCode = MCMA_AWS.HTTP_NOT_FOUND;
+        response.statusMessage = "No resource found on path '" + request.path + "'.";
+        return;
+    } 
+
+    await table.delete("JobAssignment", jobAssignmentId);
+}
+
+// Initializing rest controller for API Gateway Endpoint
+let restController = new MCMA_AWS.RestController();
+
+// adding routes
+restController.addRoute("GET", "/job-assignments", getJobAssignments);
+restController.addRoute("POST", "/job-assignments", addJobAssignment);
+restController.addRoute("GET", "/job-assignments/{id}", getJobAssignment);
+restController.addRoute("PUT", "/job-assignments/{id}", putJobAssignment);
+restController.addRoute("DELETE", "/job-assignments/{id}", deleteJobAssignment);
+
+exports.handler = async (event, context) => {
+    console.log(JSON.stringify(event, null, 2), JSON.stringify(context, null, 2));
+
+    return await restController.handleRequest(event, context);
+}
