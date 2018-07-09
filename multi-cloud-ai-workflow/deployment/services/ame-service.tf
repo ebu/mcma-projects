b9fd0c2c09ec5792a3cac96f5584323cab7859d6
Joost Rovers <joost.rovers@glookast.com>
2018-07-09 11:50:17 +0100

    Added lambda 'worker' to ame-service. This lambda function will do the metadata extraction as doing this in the api-handler would block the REST API until the work is complete.

--- multi-cloud-ai-workflow/deployment/services/ame-service.tf
+++ multi-cloud-ai-workflow/deployment/services/ame-service.tf
@@ -3,11 +3,26 @@
 #################################
 
 resource "aws_lambda_function" "ame-service-api-handler" {
-  filename         = "./../services/ame-service/dist/lambda.zip"
+  filename         = "./../services/ame-service/api-handler/dist/lambda.zip"
   function_name    = "${format("%.64s", "${var.global_prefix}-ame-service-api-handler")}"
   role             = "${aws_iam_role.iam_for_exec_lambda.arn}"
   handler          = "index.handler"
-  source_code_hash = "${base64sha256(file("./../services/ame-service/dist/lambda.zip"))}"
+  source_code_hash = "${base64sha256(file("./../services/ame-service/api-handler/dist/lambda.zip"))}"
+  runtime          = "nodejs8.10"
+  timeout          = "30"
+  memory_size      = "256"
+}
+
+#################################
+#  aws_lambda_function : ame-service-worker
+#################################
+
+resource "aws_lambda_function" "ame-service-worker" {
+  filename         = "./../services/ame-service/worker/dist/lambda.zip"
+  function_name    = "${format("%.64s", "${var.global_prefix}-ame-service-worker")}"
+  role             = "${aws_iam_role.iam_for_exec_lambda.arn}"
+  handler          = "index.handler"
+  source_code_hash = "${base64sha256(file("./../services/ame-service/worker/dist/lambda.zip"))}"
   runtime          = "nodejs8.10"
   timeout          = "30"
   memory_size      = "256"
@@ -88,7 +103,8 @@ resource "aws_api_gateway_deployment" "ame_service_deployment" {
   stage_name  = "${var.environment_type}"
 
   variables = {
-    "TableName" = "${var.global_prefix}-ame-service"
-    "PublicUrl" = "https://${aws_api_gateway_rest_api.ame_service_api.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment_type}"
+    "TableName"                = "${var.global_prefix}-ame-service"
+    "PublicUrl"                = "https://${aws_api_gateway_rest_api.ame_service_api.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment_type}"
+    "WorkerLambdaFunctionName" = "${aws_lambda_function.ame-service-worker.function_name}"
   }
 }
---
+++ multi-cloud-ai-workflow/services/ame-service/worker/src/index.js
@@ -0,0 +1,3 @@
+exports.handler = async (event, context) => {
+    console.log(JSON.stringify(event, null, 2), JSON.stringify(context, null, 2));
+}
