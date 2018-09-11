7e88f2e983802f47c92b9a29798b48cac72edfe6
Joost Rovers <joost.rovers@glookast.com>
2018-09-11 17:23:58 +0100

    Website: Added refresh button on monitor page that reloads the workflow jobs. Made layout less jumpy.

--- multi-cloud-ai-workflow/website/src/app/monitor/monitor.component.ts
+++ multi-cloud-ai-workflow/website/src/app/monitor/monitor.component.ts
@@ -5,31 +5,35 @@ import { tap } from 'rxjs/operators';
 import { WorkflowJob } from 'mcma-core';
 
 import { WorkflowService } from '../services/workflow.service';
-  
+
 @Component({
-  selector: 'mcma-monitor',
-  templateUrl: './monitor.component.html',
-  styleUrls: ['./monitor.component.scss']
+    selector: 'mcma-monitor',
+    templateUrl: './monitor.component.html',
+    styleUrls: ['./monitor.component.scss']
 })
 export class MonitorComponent implements OnInit {
-  workflowJobs$: Observable<WorkflowJob>;
+    workflowJobs$: Observable<WorkflowJob>;
 
-  private selectedWorkflowJobSubject = new BehaviorSubject<WorkflowJob>(null);
-  selectedWorkflowJob$ = this.selectedWorkflowJobSubject.asObservable();
+    private selectedWorkflowJobSubject = new BehaviorSubject<WorkflowJob>(null);
+    selectedWorkflowJob$ = this.selectedWorkflowJobSubject.asObservable();
 
-  constructor(private workflowService: WorkflowService) { }
+    constructor(private workflowService: WorkflowService) { }
 
-  ngOnInit() {
-    this.workflowJobs$ =
-      this.workflowService.getWorkflowJobs().pipe(
-        tap(jobs => {
-          if (jobs && jobs.length > 0) {
-            this.selectedWorkflowJobSubject.next(jobs[0]);
-          }
-        }));
-  }
+    ngOnInit() {
+        this.refresh();
+    }
 
-  onJobSelected(workflowJob: WorkflowJob): void {
-    this.selectedWorkflowJobSubject.next(workflowJob);
-  }
+    refresh() {
+        this.workflowJobs$ =
+            this.workflowService.getWorkflowJobs().pipe(
+                tap(jobs => {
+                    if (jobs && jobs.length > 0) {
+                        this.selectedWorkflowJobSubject.next(jobs[0]);
+                    }
+                }));
+    }
+
+    onJobSelected(workflowJob: WorkflowJob): void {
+        this.selectedWorkflowJobSubject.next(workflowJob);
+    }
 }
