6f29f47e52bc7c326812814f68da249097ba77de
Joost Rovers <joost.rovers@glookast.com>
2018-09-12 16:57:37 +0100

    Selected workflow is now highlighted in table.

--- multi-cloud-ai-workflow/website/src/app/monitor/monitor-queue/monitor-queue.component.ts
+++ multi-cloud-ai-workflow/website/src/app/monitor/monitor-queue/monitor-queue.component.ts
@@ -1,46 +1,53 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { WorkflowJob } from 'mcma-core';
 import { ThemePalette } from '@angular/material';
 
 @Component({
-  selector: 'mcma-monitor-queue',
-  styleUrls: ['./monitor-queue.component.scss'],
-  templateUrl: './monitor-queue.component.html',
+    selector: 'mcma-monitor-queue',
+    styleUrls: ['./monitor-queue.component.scss'],
+    templateUrl: './monitor-queue.component.html',
 })
 export class MonitorQueueComponent {
-  readonly displayedColumns = ['title', 'filename', 'status'];
+    readonly displayedColumns = ['title', 'filename', 'status'];
 
-  @Input() workflowJobs: WorkflowJob[];
-  @Output() jobSelected = new EventEmitter<WorkflowJob>(null);
+    selectedJob;
 
-  getStatusIcon(status: string): string {
-    if (!status) {
-      return '';
+    @Input() workflowJobs: WorkflowJob[];
+    @Output() jobSelected = new EventEmitter<WorkflowJob>(null);
+
+    getStatusIcon(status: string): string {
+        if (!status) {
+            return '';
+        }
+
+        switch (status.toUpperCase()) {
+            case 'RUNNING':
+                return 'play';
+            case 'COMPLETED':
+                return 'check';
+            case 'FAILED':
+                return 'error';
+        }
     }
 
-    switch (status.toUpperCase()) {
-      case 'RUNNING':
-        return 'play';
-      case 'COMPLETED':
-        return 'check';
-      case 'FAILED':
-        return 'error';
-    }
-  }
+    getStatusColor(status: string): ThemePalette {
+        if (!status) {
+            return null;
+        }
 
-  getStatusColor(status: string): ThemePalette {
-    if (!status) {
-      return null;
+        switch (status.toUpperCase()) {
+            case 'RUNNING':
+                return 'primary';
+            case 'COMPLETED':
+                return 'accent';
+            case 'FAILED':
+                return 'warn';
+        }
     }
 
-    switch (status.toUpperCase()) {
-      case 'RUNNING':
-        return 'primary';
-      case 'COMPLETED':
-        return 'accent';
-      case 'FAILED':
-        return 'warn';
+    selectJob(row) {
+        this.selectedJob = row;
+        this.jobSelected.emit(row)
     }
-  }
 }
