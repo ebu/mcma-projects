6ba8551194b0d0e877801946a7f1852f860ba279
Joost Rovers <joost.rovers@glookast.com>
2018-09-12 15:26:29 +0100

    Now Showing azure obtained metadata. Separated AWS and Azure obtained metadata in separate tabs.

--- multi-cloud-ai-workflow/website/src/app/monitor/monitor-detail/monitor-detail.component.ts
+++ multi-cloud-ai-workflow/website/src/app/monitor/monitor-detail/monitor-detail.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, ModuleWithComponentFactories, ViewChild, ElementRef } from '@angular/core';
 
 import { WorkflowJob } from 'mcma-core';
-import { timeout } from 'q';
 
 const MCMA_CORE = require('mcma-core');
 
@@ -13,16 +12,25 @@ const MCMA_CORE = require('mcma-core');
 export class MonitorDetailComponent implements OnChanges {
     @Input() workflowJob: WorkflowJob;
 
+    @ViewChild("videoElement")
+    private videoElementRef: ElementRef;
+
     mediaFileUrl: string = "";
 
     aiWorkflowJobStatus: string = "";
-    transcription: string = "";
-    translation: string = "";
+
+    awsTranscription: string = "";
+    awsTranslation: string = "";
+
+    azureTranscription: string = "";
+    azureCelebrities = [];
+    celebrityColumns = ['name'];
+    selectedAzureCelebrity: any = {};
 
     ngOnChanges() {
         this.aiWorkflowJobStatus = "Unknown";
-        this.transcription = "";
-        this.translation = "";
+        this.awsTranscription = "";
+        this.awsTranslation = "";
 
         if (this.workflowJob &&
             this.workflowJob.jobOutput &&
@@ -73,33 +81,103 @@ export class MonitorDetailComponent implements OnChanges {
 
             let defaultMessage = !isDone ? "Waiting for results" : "No results";
 
+            this.awsTranscription = defaultMessage;
+            this.awsTranslation = defaultMessage;
+            this.azureTranscription = defaultMessage;
+
             if (bmContent &&
                 bmContent.awsAiMetadata &&
                 bmContent.awsAiMetadata.transcription) {
 
                 if (bmContent.awsAiMetadata.transcription.original) {
-                    this.transcription = bmContent.awsAiMetadata.transcription.original;
-                } else {
-                    this.transcription = defaultMessage;
+                    this.awsTranscription = bmContent.awsAiMetadata.transcription.original;
                 }
-
                 if (bmContent.awsAiMetadata.transcription.translation) {
-                    this.translation = bmContent.awsAiMetadata.transcription.translation;
-                } else {
-                    this.translation = defaultMessage;
+                    this.awsTranslation = bmContent.awsAiMetadata.transcription.translation;
                 }
-            } else {
-                this.transcription = defaultMessage;
-                this.translation = defaultMessage;
+            }
+
+            let celebrities = [];
+
+            if (bmContent &&
+                bmContent.azureAiMetadata) {
+                const data = bmContent.azureAiMetadata;
+                if (data.videos) {
+                    for (const video of data.videos) {
+                        if (video.insights) {
+                            if (video.insights.transcript) {
+
+                                let azureTranscription = "";
+
+                                for (const transcript of video.insights.transcript) {
+                                    if (transcript.text) {
+                                        azureTranscription += transcript.text + " ";
+                                    }
+                                }
+
+                                this.azureTranscription = this.azureTranscription.trim();
+
+                                if (azureTranscription) {
+                                    this.azureTranscription = azureTranscription;
+                                }
+                            }
+
+                            if (video.insights.faces) {
+                                for (const face of video.insights.faces) {
+                                    if (face.imageUrl) {
+                                        celebrities.push(face);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            let selectedAzureCelebrity = this.selectedAzureCelebrity;
+            this.selectedAzureCelebrity = {};
+
+            this.azureCelebrities = celebrities;
+
+            for (const celebrity of celebrities) {
+                if (celebrity.imageUrl === selectedAzureCelebrity.imageUrl) {
+                    this.selectAzureCelebrity = celebrity;
+                }
+            }
+
+            if (!this.selectedAzureCelebrity.imageUrl && celebrities.length) {
+                this.selectedAzureCelebrity = celebrities[0];
             }
 
             if (!isDone) {
-                window.setTimeout(() => this.loadAiData(conformWorkflowId), 3000);
+                window.setTimeout(() => this.loadAiData(conformWorkflowId), 5000);
             }
         } catch (error) {
-            this.transcription = "Error while retrieving data";
-            this.translation = "Error while retrieving data";
+            this.awsTranscription = "Error while retrieving data";
+            this.awsTranslation = "Error while retrieving data";
             console.log("Failed to retrieve data");
         }
+    }
+
+    selectAzureCelebrity(row) {
+        console.log(row);
+        this.selectedAzureCelebrity = row;
+    }
+
+    seekVideoAzure(instance) {
+        console.log(instance);
+
+        let time = instance.start;
+        let timeParts = time.split(":");
+
+        let timeSeconds = 0;
+
+        for (const timePart of timeParts) {
+            let parsed = Number.parseFloat(timePart);
+            timeSeconds *= 60;
+            timeSeconds += parsed;
+        }
+
+        this.videoElementRef.nativeElement.currentTime = timeSeconds;
     }
 }
