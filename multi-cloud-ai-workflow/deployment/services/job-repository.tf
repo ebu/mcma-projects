d5bf511702352c74270819ca9dbb9c557b3fe886
Evan Verney-Fink <evan@triskel-inc.com>
2018-12-11 18:27:54 -0500

    Add support for CORS via OPTIONS endpoints in relevant APIs

--- multi-cloud-ai-workflow/deployment/services/job-repository.tf
+++ multi-cloud-ai-workflow/deployment/services/job-repository.tf
@@ -67,6 +67,55 @@ resource "aws_api_gateway_resource" "job_repository_api_resource" {
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_method" "job_repository_options_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.job_repository_api.id}"
+  resource_id   = "${aws_api_gateway_resource.job_repository_api_resource.id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "job_repository_options_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.job_repository_api.id}"
+  resource_id   = "${aws_api_gateway_resource.job_repository_api_resource.id}"
+  http_method   = "${aws_api_gateway_method.job_repository_options_method.http_method}"
+  status_code   = "200"
+  response_models = {
+      "application/json" = "Empty"
+  }
+  response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true,
+      "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "job_repository_options_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.job_repository_api.id}"
+  resource_id   = "${aws_api_gateway_resource.job_repository_api_resource.id}"
+  http_method   = "${aws_api_gateway_method.job_repository_options_method.http_method}"
+  type          = "MOCK"
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "job_repository_options_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.job_repository_api.id}"
+  resource_id   = "${aws_api_gateway_resource.job_repository_api_resource.id}"
+  http_method   = "${aws_api_gateway_method.job_repository_options_method.http_method}"
+  status_code   = "${aws_api_gateway_method_response.job_repository_options_200.status_code}"
+  response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+      "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,PATCH,DELETE'",
+      "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
 resource "aws_api_gateway_method" "job_repository_api_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.job_repository_api.id}"
   resource_id   = "${aws_api_gateway_resource.job_repository_api_resource.id}"
