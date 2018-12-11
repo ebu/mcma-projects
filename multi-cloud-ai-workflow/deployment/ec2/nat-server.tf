458e7e0d1a48cd83f89aea48303679d57ddffa06
Evan Verney-Fink <evan@triskel-inc.com>
2018-12-11 18:24:19 -0500

    Fix for security group issue that destroyed/created ec2s every time

--- multi-cloud-ai-workflow/deployment/ec2/nat-server.tf
+++ multi-cloud-ai-workflow/deployment/ec2/nat-server.tf
@@ -1,11 +1,13 @@
 /* NAT server */
 resource "aws_instance" "nat" {
-  ami               = "${lookup(var.amis, var.aws_region)}"
-  instance_type     = "${var.aws_instance_type}"
-  subnet_id         = "${aws_subnet.public.id}"
-  security_groups   = ["${aws_security_group.default.id}", "${aws_security_group.nat.id}"]
-  key_name          = "${aws_key_pair.deployer.key_name}"
-  source_dest_check = false
+  ami                      = "${lookup(var.amis, var.aws_region)}"
+  instance_type            = "${var.aws_instance_type}"
+  subnet_id                = "${aws_subnet.public.id}"
+  // we use "vpc_security_group_ids" instead of "security_groups" here to prevent destruction & recreation of the EC2 instance every time:
+  // https://github.com/hashicorp/terraform/issues/7221
+  vpc_security_group_ids   = ["${aws_security_group.default.id}", "${aws_security_group.nat.id}"]
+  key_name                 = "${aws_key_pair.deployer.key_name}"
+  source_dest_check        = false
 
   tags = {
     Name = "${var.global_prefix}-nat"
