766dc0ad28ce56342f91691c3fb8486ff1222465
Joost Rovers <joost.rovers@glookast.com>
2019-05-08 16:49:40 +0100

    Added deployment scripts for FFmpeg Transform Service

---
+++ multi-cloud-ai-workflow/services/transform-service/deployment/variables.tf
@@ -0,0 +1,24 @@
+#########################
+# Environment Variables
+#########################
+
+variable "environment_name" {}
+variable "environment_type" {}
+variable "global_prefix" {}
+
+#########################
+# AWS Variables
+#########################
+
+variable "aws_account_id" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+#########################
+# Service Registry Variables
+#########################
+
+variable "services_url" {}
+variable "services_auth_type" {}
+variable "services_auth_context" {}
