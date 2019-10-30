5bda1cdf94f4081ad43ba9742149e6c13720d0ff
Bruno Felicio <bruno.felicio@mail.com>
2019-10-30 16:26:05 +0100

    Google Cloud AI integration (WIP)

--- multi-cloud-ai-workflow/services/google-ai-service/worker/src/profiles/extract-audio.js
+++ multi-cloud-ai-workflow/services/google-ai-service/worker/src/profiles/extract-audio.js
@@ -13,14 +13,9 @@ const S3PutObject = util.promisify(S3.putObject.bind(S3));
 const { Logger, Locator } = require("mcma-core");
 const { ffmpeg } = require("../ffmpeg");
 
+const speech = require("@google-cloud/speech");
 const { Storage } = require('@google-cloud/storage');
-const storage = new Storage({
-    credentials: {
-        client_email: 'mcma-storage-admin@mcma-sandbox.iam.gserviceaccount.com',
-        private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZFBKFxk9J5FF7\ngLGVdIqB7qSHFql0IqNfaTYx82fS+HAm5yLn1eDZ8+WebnNQrjr2UpJ5xIl9m8G8\nbcLEwyXymaGqXMMT0WTr8JolqPoQWHDUGVKvwSDlq8R4XH79rwcCUjdOJuQ+JX/4\nDqFm4pNp9GjlYXtHqQhZpr3CtVN1bD4G4Js4kNNc+TEv4d+G/Kh3E2599hoYibIk\nApEWijxh6ki87qTCt7Us976mZNXUzmVaOJlRe88BjXe9aoUu1ZTXds16S3s9Iqps\nrSd9Ie7ZfX27Sea9yT345iCTwXa1x4pmpJ3wuzCTko3YjoDtcJpI1ezXlSuai3yM\nB7/RUkCjAgMBAAECggEAQR4LRRHXb7jquo1Dva5+Lwh7v6Vxvx/u/GrOrY/75gBe\nAQaI0DMou75nOq6Ealk7ZsY4XgkvbZng342XcUkMbvdQkJ52M4t7EWzfvc50VZix\nomAFTxNqMZkQchzyvmqCokekFCAyLoeYOMbMfO48RDG+n3kIzrKGhVCLX3TwBD42\n8w+e/OujowDPfRDzmbhTU0UhLIZ0r0jIxZjhE7131VxQ7jK9Q1LNCPXxzYgk7azZ\nHqtbuukU6QQFgC+c0HIZgHxcVXulZ830il4z7T9XkAIIxbn+aUQmtPmoNFn7cZF2\nbaAYwNthvYMds6/3g+zbSDJmqL2TGxAY9K4j2oxUuQKBgQDWDDmFuSGfcboDwIed\nkjmFikYpzZG+Bqqdywl+dPcwTygW+V3oh1AVJJpAwEvc+06/wNmC2x3YCMgQzi1y\nlkCzkX/CkdzZ+mnop5e8r4byETEFOsbXHr4haUTPz/5MnqRGxjU1nGXE7q3lPR7o\nZjRyTWOffRjX08Lu4psN1kOL+wKBgQC3FLscaqTBfMP8q938kL/gS1/57mR3yRzP\nR+tVd2N+bVOCDFUHmmUJcp0mAbzroM+uPDtB/GUZ8XUsMzZllYwm/NN3Q+kRiudU\nEyz8itOKdYno2/I/nxfk5iC7p+eoilOR3yhMPEFg2vw4L2UwoICbU16Dxy3d9zq/\n0d1FMQSVeQKBgHR2Dk4t3pdsP3OPK7qWNunrxCU2jlfANNDKCg5eAaitSPjHEUtV\nZEM8uRkGB8Dik4RNn4tNQT2r+X77T0sLaYTKY6MvzeRkFX/av/qP3nKS3AMmac3e\nEkEHj8MqPgjeQC+p5IWY5N5zpvYVfV2hTmPqq4u5euzjcSqc7RRoQjRFAoGAdRBT\naCMkf/e4FasLgUBJADESCGyPXyyAOGXjKz3wp1leaSxbfkYRzs2zgCbiVsP3p5Ap\nz9KEDueepYmFgcTy3ccRrBQHRklMCy1LuZtHEiR/7x4cMuQWUi13xqXg8ZqpPv+y\n87Qflb/QQFjF2ib2tNrE93v3bxNzRE37rw9dv/kCgYEAnhXyUlcDdMTK2HvWPU1c\nPovX36aK9cHKoBeS7G2vypwezEsTGbA8kXghQApoKXvsnvg+rD3F93ujtHpkLeNH\nOUWmbe8e9RGAqlZ58sEfCCIvpRe5Q4ir5igyc6cpEH2i0sO4np2wejUbwnVHDcML\nCqB9uAWzB/gICp2g/5G6Cu8=\n-----END PRIVATE KEY-----\n',
-    },
-    projectId: "mcma-sandbox"
-});
+
 
 
 async function extractAudio(workerJobHelper) {
@@ -31,7 +26,14 @@ async function extractAudio(workerJobHelper) {
 
     const jobInput = workerJobHelper.getJobInput();
     const googleProjectId = workerJobHelper.getRequest().getRequiredContextVariable("googleProjectId");
+    const googleBucketName = workerJobHelper.getRequest().getRequiredContextVariable("googleBucketName");
+    const googleClientEmail = workerJobHelper.getRequest().getRequiredContextVariable("googleClientEmail");
+    const googlePrivateKey = workerJobHelper.getRequest().getRequiredContextVariable("googlePrivateKey");
+
     console.log('googleProjectId', googleProjectId);
+    console.log('googleBucketName', googleBucketName);
+    console.log('googleClientEmail', googleClientEmail);
+    console.log('googlePrivateKey', googlePrivateKey);
 
     Logger.debug("61. Extract audio track from mp4 source using ffmpeg");
     const inputFile = jobInput.inputFile;
@@ -56,34 +58,48 @@ async function extractAudio(workerJobHelper) {
 
         await ffmpeg(params_extract_audio);
 
-        const bucketName = 'mcma-gcp-speech-to-text';
+        console.log("#####################");
+        console.log("GOOGLE BUCKET - START");
+        console.log("#####################");
+
+        const storage = new Storage({
+            credentials: {
+                client_email: googleClientEmail,
+                private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZFBKFxk9J5FF7\ngLGVdIqB7qSHFql0IqNfaTYx82fS+HAm5yLn1eDZ8+WebnNQrjr2UpJ5xIl9m8G8\nbcLEwyXymaGqXMMT0WTr8JolqPoQWHDUGVKvwSDlq8R4XH79rwcCUjdOJuQ+JX/4\nDqFm4pNp9GjlYXtHqQhZpr3CtVN1bD4G4Js4kNNc+TEv4d+G/Kh3E2599hoYibIk\nApEWijxh6ki87qTCt7Us976mZNXUzmVaOJlRe88BjXe9aoUu1ZTXds16S3s9Iqps\nrSd9Ie7ZfX27Sea9yT345iCTwXa1x4pmpJ3wuzCTko3YjoDtcJpI1ezXlSuai3yM\nB7/RUkCjAgMBAAECggEAQR4LRRHXb7jquo1Dva5+Lwh7v6Vxvx/u/GrOrY/75gBe\nAQaI0DMou75nOq6Ealk7ZsY4XgkvbZng342XcUkMbvdQkJ52M4t7EWzfvc50VZix\nomAFTxNqMZkQchzyvmqCokekFCAyLoeYOMbMfO48RDG+n3kIzrKGhVCLX3TwBD42\n8w+e/OujowDPfRDzmbhTU0UhLIZ0r0jIxZjhE7131VxQ7jK9Q1LNCPXxzYgk7azZ\nHqtbuukU6QQFgC+c0HIZgHxcVXulZ830il4z7T9XkAIIxbn+aUQmtPmoNFn7cZF2\nbaAYwNthvYMds6/3g+zbSDJmqL2TGxAY9K4j2oxUuQKBgQDWDDmFuSGfcboDwIed\nkjmFikYpzZG+Bqqdywl+dPcwTygW+V3oh1AVJJpAwEvc+06/wNmC2x3YCMgQzi1y\nlkCzkX/CkdzZ+mnop5e8r4byETEFOsbXHr4haUTPz/5MnqRGxjU1nGXE7q3lPR7o\nZjRyTWOffRjX08Lu4psN1kOL+wKBgQC3FLscaqTBfMP8q938kL/gS1/57mR3yRzP\nR+tVd2N+bVOCDFUHmmUJcp0mAbzroM+uPDtB/GUZ8XUsMzZllYwm/NN3Q+kRiudU\nEyz8itOKdYno2/I/nxfk5iC7p+eoilOR3yhMPEFg2vw4L2UwoICbU16Dxy3d9zq/\n0d1FMQSVeQKBgHR2Dk4t3pdsP3OPK7qWNunrxCU2jlfANNDKCg5eAaitSPjHEUtV\nZEM8uRkGB8Dik4RNn4tNQT2r+X77T0sLaYTKY6MvzeRkFX/av/qP3nKS3AMmac3e\nEkEHj8MqPgjeQC+p5IWY5N5zpvYVfV2hTmPqq4u5euzjcSqc7RRoQjRFAoGAdRBT\naCMkf/e4FasLgUBJADESCGyPXyyAOGXjKz3wp1leaSxbfkYRzs2zgCbiVsP3p5Ap\nz9KEDueepYmFgcTy3ccRrBQHRklMCy1LuZtHEiR/7x4cMuQWUi13xqXg8ZqpPv+y\n87Qflb/QQFjF2ib2tNrE93v3bxNzRE37rw9dv/kCgYEAnhXyUlcDdMTK2HvWPU1c\nPovX36aK9cHKoBeS7G2vypwezEsTGbA8kXghQApoKXvsnvg+rD3F93ujtHpkLeNH\nOUWmbe8e9RGAqlZ58sEfCCIvpRe5Q4ir5igyc6cpEH2i0sO4np2wejUbwnVHDcML\nCqB9uAWzB/gICp2g/5G6Cu8=\n-----END PRIVATE KEY-----\n',
+            },
+            projectId: googleProjectId
+        });
 
         let bucketExists = false;
         await storage.getBuckets().then(buckets => {
             for (let bucket of buckets[0]) {
-                if (bucket['id'] === bucketName) {
+                if (bucket['id'] === googleBucketName) {
                     bucketExists = true;
                 }
             }
         });
 
         if (bucketExists === false) {
-            await storage.createBucket(bucketName, {
+            await storage.createBucket(googleBucketName, {
                 location: 'eu',
                 storageClass: 'Standard',
             });
-            console.log(`Bucket ${bucketName} created.`);
+            console.log(`Bucket ${googleBucketName} created.`);
         } else {
-            console.log(`Bucket ${bucketName} already exists.`);
+            console.log(`Bucket ${googleBucketName} already exists.`);
         }
 
-        await storage.bucket(bucketName).upload(output, {
+        await storage.bucket(googleBucketName).upload(output, {
             resumable: false,
             metadata: {
                 cacheControl: 'public, max-age=31536000',
             },
         });
-        Logger.debug(`61.5. ${output} uploaded to ${bucketName}.`);
+        Logger.debug(`61.5. ${output} uploaded to ${googleBucketName}.`);
+
+        console.log("###################");
+        console.log("GOOGLE BUCKET - END");
+        console.log("###################");
 
         Logger.debug("61.6. removing file from ffmpeg local temp repo");
         await fsUnlink(input);
@@ -100,6 +116,57 @@ async function extractAudio(workerJobHelper) {
         Body: await fsReadFile(output)
     };
     await S3PutObject(s3Params);
+
+    console.log("######################");
+    console.log("Speech to Text - START");
+    console.log("######################");
+
+    const client = new speech.SpeechClient({
+        credentials: {
+            client_email: googleClientEmail,
+            private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZFBKFxk9J5FF7\ngLGVdIqB7qSHFql0IqNfaTYx82fS+HAm5yLn1eDZ8+WebnNQrjr2UpJ5xIl9m8G8\nbcLEwyXymaGqXMMT0WTr8JolqPoQWHDUGVKvwSDlq8R4XH79rwcCUjdOJuQ+JX/4\nDqFm4pNp9GjlYXtHqQhZpr3CtVN1bD4G4Js4kNNc+TEv4d+G/Kh3E2599hoYibIk\nApEWijxh6ki87qTCt7Us976mZNXUzmVaOJlRe88BjXe9aoUu1ZTXds16S3s9Iqps\nrSd9Ie7ZfX27Sea9yT345iCTwXa1x4pmpJ3wuzCTko3YjoDtcJpI1ezXlSuai3yM\nB7/RUkCjAgMBAAECggEAQR4LRRHXb7jquo1Dva5+Lwh7v6Vxvx/u/GrOrY/75gBe\nAQaI0DMou75nOq6Ealk7ZsY4XgkvbZng342XcUkMbvdQkJ52M4t7EWzfvc50VZix\nomAFTxNqMZkQchzyvmqCokekFCAyLoeYOMbMfO48RDG+n3kIzrKGhVCLX3TwBD42\n8w+e/OujowDPfRDzmbhTU0UhLIZ0r0jIxZjhE7131VxQ7jK9Q1LNCPXxzYgk7azZ\nHqtbuukU6QQFgC+c0HIZgHxcVXulZ830il4z7T9XkAIIxbn+aUQmtPmoNFn7cZF2\nbaAYwNthvYMds6/3g+zbSDJmqL2TGxAY9K4j2oxUuQKBgQDWDDmFuSGfcboDwIed\nkjmFikYpzZG+Bqqdywl+dPcwTygW+V3oh1AVJJpAwEvc+06/wNmC2x3YCMgQzi1y\nlkCzkX/CkdzZ+mnop5e8r4byETEFOsbXHr4haUTPz/5MnqRGxjU1nGXE7q3lPR7o\nZjRyTWOffRjX08Lu4psN1kOL+wKBgQC3FLscaqTBfMP8q938kL/gS1/57mR3yRzP\nR+tVd2N+bVOCDFUHmmUJcp0mAbzroM+uPDtB/GUZ8XUsMzZllYwm/NN3Q+kRiudU\nEyz8itOKdYno2/I/nxfk5iC7p+eoilOR3yhMPEFg2vw4L2UwoICbU16Dxy3d9zq/\n0d1FMQSVeQKBgHR2Dk4t3pdsP3OPK7qWNunrxCU2jlfANNDKCg5eAaitSPjHEUtV\nZEM8uRkGB8Dik4RNn4tNQT2r+X77T0sLaYTKY6MvzeRkFX/av/qP3nKS3AMmac3e\nEkEHj8MqPgjeQC+p5IWY5N5zpvYVfV2hTmPqq4u5euzjcSqc7RRoQjRFAoGAdRBT\naCMkf/e4FasLgUBJADESCGyPXyyAOGXjKz3wp1leaSxbfkYRzs2zgCbiVsP3p5Ap\nz9KEDueepYmFgcTy3ccRrBQHRklMCy1LuZtHEiR/7x4cMuQWUi13xqXg8ZqpPv+y\n87Qflb/QQFjF2ib2tNrE93v3bxNzRE37rw9dv/kCgYEAnhXyUlcDdMTK2HvWPU1c\nPovX36aK9cHKoBeS7G2vypwezEsTGbA8kXghQApoKXvsnvg+rD3F93ujtHpkLeNH\nOUWmbe8e9RGAqlZ58sEfCCIvpRe5Q4ir5igyc6cpEH2i0sO4np2wejUbwnVHDcML\nCqB9uAWzB/gICp2g/5G6Cu8=\n-----END PRIVATE KEY-----\n',
+        },
+        projectId: googleProjectId
+    });
+
+    const gcsUri = 'gs://mcma-gcp-speech-to-text/extractedAudio.flac';
+    const encoding = 'FLAC';
+    const sampleRateHertz = 48000;
+    const languageCode = 'en-US';
+
+    const config = {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+        audioChannelCount: 2,
+    };
+
+    const audio = {
+        uri: gcsUri,
+    };
+
+    const request = {
+        config: config,
+        audio: audio,
+    };
+
+    const [operation] = await client.longRunningRecognize(request);
+
+    const [response] = await operation.promise();
+    console.log(`response: ${response}`);
+
+    const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+    console.log(`Transcription: ${transcription}`);
+
+    const projectId = client.getProjectId();
+    console.log(projectId);
+
+    console.log("##################");
+    console.log("Speech to Text END");
+    console.log("##################");
+
 
     // 9. updating JobAssignment with jobOutput
     Logger.debug("61.8. Associate output location with job output");
--- multi-cloud-ai-workflow/workflows/ai/62-register-extract-audio-google/src/index.js
+++ multi-cloud-ai-workflow/workflows/ai/62-register-extract-audio-google/src/index.js
@@ -2,7 +2,6 @@
 
 // require
 const util = require("util");
-const speech = require("@google-cloud/speech");
 
 
 const AWS = require("aws-sdk");
@@ -78,70 +77,17 @@ exports.handler = async (event, context) => {
     console.log("httpEndpoint_web", httpEndpoint_web);
     console.log("mediaFileUri", mediaFileUri);
 
-    // Speech to Text
-    console.log("##############");
-    console.log("Speech to Text");
-    console.log("##############");
 
-    // Creates a client
-    const client = new speech.SpeechClient({
-        credentials: {
-            client_email: 'mcma-storage-admin@mcma-sandbox.iam.gserviceaccount.com',
-            private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZFBKFxk9J5FF7\ngLGVdIqB7qSHFql0IqNfaTYx82fS+HAm5yLn1eDZ8+WebnNQrjr2UpJ5xIl9m8G8\nbcLEwyXymaGqXMMT0WTr8JolqPoQWHDUGVKvwSDlq8R4XH79rwcCUjdOJuQ+JX/4\nDqFm4pNp9GjlYXtHqQhZpr3CtVN1bD4G4Js4kNNc+TEv4d+G/Kh3E2599hoYibIk\nApEWijxh6ki87qTCt7Us976mZNXUzmVaOJlRe88BjXe9aoUu1ZTXds16S3s9Iqps\nrSd9Ie7ZfX27Sea9yT345iCTwXa1x4pmpJ3wuzCTko3YjoDtcJpI1ezXlSuai3yM\nB7/RUkCjAgMBAAECggEAQR4LRRHXb7jquo1Dva5+Lwh7v6Vxvx/u/GrOrY/75gBe\nAQaI0DMou75nOq6Ealk7ZsY4XgkvbZng342XcUkMbvdQkJ52M4t7EWzfvc50VZix\nomAFTxNqMZkQchzyvmqCokekFCAyLoeYOMbMfO48RDG+n3kIzrKGhVCLX3TwBD42\n8w+e/OujowDPfRDzmbhTU0UhLIZ0r0jIxZjhE7131VxQ7jK9Q1LNCPXxzYgk7azZ\nHqtbuukU6QQFgC+c0HIZgHxcVXulZ830il4z7T9XkAIIxbn+aUQmtPmoNFn7cZF2\nbaAYwNthvYMds6/3g+zbSDJmqL2TGxAY9K4j2oxUuQKBgQDWDDmFuSGfcboDwIed\nkjmFikYpzZG+Bqqdywl+dPcwTygW+V3oh1AVJJpAwEvc+06/wNmC2x3YCMgQzi1y\nlkCzkX/CkdzZ+mnop5e8r4byETEFOsbXHr4haUTPz/5MnqRGxjU1nGXE7q3lPR7o\nZjRyTWOffRjX08Lu4psN1kOL+wKBgQC3FLscaqTBfMP8q938kL/gS1/57mR3yRzP\nR+tVd2N+bVOCDFUHmmUJcp0mAbzroM+uPDtB/GUZ8XUsMzZllYwm/NN3Q+kRiudU\nEyz8itOKdYno2/I/nxfk5iC7p+eoilOR3yhMPEFg2vw4L2UwoICbU16Dxy3d9zq/\n0d1FMQSVeQKBgHR2Dk4t3pdsP3OPK7qWNunrxCU2jlfANNDKCg5eAaitSPjHEUtV\nZEM8uRkGB8Dik4RNn4tNQT2r+X77T0sLaYTKY6MvzeRkFX/av/qP3nKS3AMmac3e\nEkEHj8MqPgjeQC+p5IWY5N5zpvYVfV2hTmPqq4u5euzjcSqc7RRoQjRFAoGAdRBT\naCMkf/e4FasLgUBJADESCGyPXyyAOGXjKz3wp1leaSxbfkYRzs2zgCbiVsP3p5Ap\nz9KEDueepYmFgcTy3ccRrBQHRklMCy1LuZtHEiR/7x4cMuQWUi13xqXg8ZqpPv+y\n87Qflb/QQFjF2ib2tNrE93v3bxNzRE37rw9dv/kCgYEAnhXyUlcDdMTK2HvWPU1c\nPovX36aK9cHKoBeS7G2vypwezEsTGbA8kXghQApoKXvsnvg+rD3F93ujtHpkLeNH\nOUWmbe8e9RGAqlZ58sEfCCIvpRe5Q4ir5igyc6cpEH2i0sO4np2wejUbwnVHDcML\nCqB9uAWzB/gICp2g/5G6Cu8=\n-----END PRIVATE KEY-----\n',
-        },
-        projectId: "mcma-sandbox"
-    });
-
-    const gcsUri = 'gs://mcma-gcp-speech-to-text/extractedAudio.flac';
-    const encoding = 'FLAC';
-    const sampleRateHertz = 48000;
-    const languageCode = 'en-US';
-
-    const config = {
-        encoding: encoding,
-        sampleRateHertz: sampleRateHertz,
-        languageCode: languageCode,
-        audioChannelCount: 2,
-    };
-
-    const audio = {
-        uri: gcsUri,
-    };
-
-    const request = {
-        config: config,
-        audio: audio,
-    };
-
-    // Detects speech in the audio file. This creates a recognition job that you
-    // can wait for now, or get its result later.
-    const [operation] = await client.longRunningRecognize(request);
-    // Get a Promise representation of the final result of the job
-    const [response] = await operation.promise();
-    console.log(`response: ${response}`);
-
-    const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\n');
-    console.log(`Transcription: ${transcription}`);
-
-    const projectId = client.getProjectId();
-    console.log(projectId);
-
-    // Speech to Text
-    console.log("##################");
-    console.log("Speech to Text END");
-    console.log("##################");
 
 
     // acquire the registered BMContent
     let bmContent = await resourceManager.resolve(event.input.bmContent);
 
-    if (!bmContent.googleAiMetadata) {
+    /*if (!bmContent.googleAiMetadata) {
         bmContent.googleAiMetadata = {};
     }
 
-    bmContent.googleAiMetadata.transcription = transcription;
+    bmContent.googleAiMetadata.transcription = transcription;*/
 
     // create BMEssence
     let locator = new Locator({
