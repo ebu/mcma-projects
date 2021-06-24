2cdab2be53ddad17369884fea987d8b6450369fe
Joost Rovers <joost@rovers.pt>
2021-06-24 12:02:34 +0100

    Added MCMA services Service Registry, Job Processor, MediaInfo AME service, and StepFunctions Workflow service

--- mam-demo/deployment/main.tf
+++ mam-demo/deployment/main.tf
@@ -7,3 +7,94 @@ provider "aws" {
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
+
+############################################
+# Cloud watch log group for central logging
+############################################
+
+resource "aws_cloudwatch_log_group" "main" {
+  name = "/mcma/${var.global_prefix}"
+}
+
+#########################
+# Service Registry Module
+#########################
+
+module "service_registry" {
+  source = "https://ch-ebu-mcma-module-repository.s3.eu-central-1.amazonaws.com/ebu/service-registry/aws/0.13.24.1/module.zip"
+
+  prefix = "${var.global_prefix}-service-registry"
+
+  stage_name = var.environment_type
+
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+
+  log_group = aws_cloudwatch_log_group.main
+
+  services = [
+    module.job_processor.service_definition,
+    module.mediainfo_ame_service.service_definition,
+    module.stepfunctions_workflow_service.service_definition,
+  ]
+}
+
+#########################
+# Job Processor Module
+#########################
+
+module "job_processor" {
+  source = "https://ch-ebu-mcma-module-repository.s3.eu-central-1.amazonaws.com/ebu/job-processor/aws/0.13.24.1/module.zip"
+
+  prefix = "${var.global_prefix}-job-processor"
+
+  stage_name     = var.environment_type
+  dashboard_name = var.global_prefix
+
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+
+  service_registry = module.service_registry
+
+  log_group = aws_cloudwatch_log_group.main
+}
+
+#########################
+# Media Info AME Service
+#########################
+
+module "mediainfo_ame_service" {
+  source = "https://ch-ebu-mcma-module-repository.s3.eu-central-1.amazonaws.com/ebu/mediainfo-ame-service/aws/0.0.1/module.zip"
+
+  prefix = "${var.global_prefix}-mediainfo-ame-service"
+
+  stage_name     = var.environment_type
+
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+
+  service_registry = module.service_registry
+
+  log_group = aws_cloudwatch_log_group.main
+}
+
+########################################
+# AWS Step Functions Workflow Service
+########################################
+
+module "stepfunctions_workflow_service" {
+  source = "https://ch-ebu-mcma-module-repository.s3.eu-central-1.amazonaws.com/ebu/step-functions-workflow-service/aws/0.0.1/module.zip"
+
+  prefix = "${var.global_prefix}-stepfunctions-workflow-service"
+
+  stage_name = var.environment_type
+
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+
+  service_registry = module.service_registry
+
+  log_group = aws_cloudwatch_log_group.main
+
+  workflows = []
+}
--- mam-demo/deployment/versions.tf
+++ mam-demo/deployment/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.24.0"
+      version = ">= 3.46.0"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 }
